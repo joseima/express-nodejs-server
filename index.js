b8e3e2e49f6f6a04afcf2d1c1bfd59a35a4466cc
joseima <joseima17@gmail.com>
2024-03-18 10:52:51 +0100

    added conditional to env port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const {logErrors, errorHandler, boomErrorHandler} = require('./middlewares/error
 
 
 const app=express();
-const port=3000;
+const port= process.env.PORT || 3000;
 
 app.use(express.json());
 
