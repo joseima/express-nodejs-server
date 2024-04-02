319fa0e65244fe7529cb1126e0f5b05a8fd50199
joseima <joseima17@gmail.com>
2024-04-02 17:37:30 +0200

    implemented shop orders, implemented relations categories-products, orders-customers-products, implemented filter queries: pagination and price-range filters

--- services/order.service.js
+++ services/order.service.js
@@ -1,32 +1,49 @@
 const boom = require('@hapi/boom');
 
+const { models } = require('./../libs/sequelize');
+
 class OrderService {
+	constructor() {
+	}
 
-  constructor(){
-  }
-  async create(data) {
-    return data;
-  }
+	async create(data) {
+		const newOrder = await models.Order.create(data);
+		return newOrder;
+	}
 
-  async find() {
-    return [];
-  }
-
-  async findOne(id) {
-    return { id };
+  async addItem(data) {
+    const newItem = await models.OrderProduct.create(data);
+		return newItem;
   }
 
-  async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
-  }
+	async find() {
+    const orders = await models.Order.findAll();
+    return orders;
+	}
 
-  async delete(id) {
-    return { id };
-  }
+	async findOne(id) {
+		const order = await models.Order.findByPk(id, {
+			include: [
+				{
+					association: 'customer',
+					include: ['user'],
+				},
+        'items'
+			],
+		});
+		return order;
+	}
 
+	async update(id, changes) {
+		return {
+			id,
+			changes,
+		};
+	}
+
+	async delete(id) {
+		return { id };
+	}
 }
 
 module.exports = OrderService;
